7f2d9d20056fce2a6d309953f34750b69ba8d03d
Clemens <59970449+K9N6GM@users.noreply.github.com>
2022-01-02 20:12:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,39 +1,79 @@
 import numpy as np
 import tensorflow as tf
-import tensorflow_probability as tfp
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 
 
-input = tf.constant([[1.]])
-y = input**2
-
-hidden_dim_1 = 3
-hidden_dim_2 = 3
-
-weights_l1 = tf.Variable(tfp.distributions.Normal(loc=0, scale=1).sample((hidden_dim_1, 1)))
-bias_l1 = tf.Variable(tfp.distributions.Normal(loc=0, scale=1).sample((hidden_dim_1, 1)))
+data_size = 10000
+batch_size = 10
+epochs = 20
+eta = 0.005
+model_neurons = [1, 100, 100, 1]
 
 
-weights_l2 = tf.Variable(tfp.distributions.Normal(
-    loc=0, scale=1).sample((hidden_dim_2, hidden_dim_1)))
-bias_l2 = tf.Variable(tfp.distributions.Normal(loc=0, scale=1).sample((hidden_dim_2, 1)))
+def data_generator(data_size):
+    x_value = np.random.normal(loc=0, scale=1, size=(data_size, 1))
+    y_value = x_value ** 2 + np.random.normal(loc=0, scale=0.1, size=(data_size, 1))
+    return x_value, y_value
 
 
-weights_l3 = tf.Variable(tfp.distributions.Normal(loc=0, scale=1).sample((1, hidden_dim_2)))
-bias_l3 = tf.Variable(tfp.distributions.Normal(loc=0, scale=1).sample((1, 1)))
+x_train, y_train = data_generator(data_size)
 
 
-with tf.GradientTape() as tape:
-    act_l1 = tf.nn.relu(tf.matmul(weights_l1, input) + bias_l1)
-    act_l2 = tf.nn.relu(tf.matmul(weights_l2, act_l1) + bias_l2)
-    output = tf.matmul(weights_l3, act_l2) + bias_l3
-    loss = 0.5 * (y - output) ** 2
+def model_loss(y, x_predicted):
+    return 0.5 * (y - x_predicted) ** 2
 
-dl_dw_l1, dl_dw_l2, dl_dw_l3, dl_db_l1, dl_db_l2, dl_db_l3 = tape.gradient(
-    loss, [weights_l1, weights_l2, weights_l3, bias_l1, bias_l2, bias_l3])
 
-weights_l1 = weights_l1 + dl_dw_l1 * tf.constant(0.01)
+input_layer = tf.keras.Input(shape=(model_neurons[0],))
+dense_layer = tf.keras.layers.Dense(model_neurons[1], activation='relu')(input_layer)
+dense_layer = tf.keras.layers.Dense(model_neurons[2], activation='relu')(dense_layer)
+dense_layer = tf.keras.layers.Dense(model_neurons[3])(dense_layer)
 
-print(weights_l1)
-#print('Gradient Weights Layer 1:', dl_dw_l1, dl_db_l1, dl_dw_l2, dl_db_l2, dl_dw_l3, dl_db_l3)
+model = tf.keras.Model(input_layer, dense_layer, name='Model')
+
+model.compile(loss=model_loss)
+model.summary()
+
+
+def step(x, y):
+    with tf.GradientTape(persistent=True) as tape:
+
+        pred = model(x)
+        loss = model_loss(y, pred)
+
+    parameters = model.trainable_variables
+    gradients_loss = tape.gradient(loss, parameters)
+#    gradients_model = tape.gradient(pred, parameters)
+
+#    print(gradients_model)
+
+    eta_g = [g * eta for g in gradients_loss]
+
+    model.set_weights([p - e_g for (p, e_g) in zip(parameters, eta_g)])
+
+
+num_updates = int(data_size / batch_size)
+
+for epoch in range(epochs):
+    print('Epoch {}/{}'.format(epoch+1, epochs))
+
+    for i in range(num_updates):
+
+        start = i * batch_size
+        end = start + batch_size
+
+        step(x_train[start:end], y_train[start:end])
+
+
+f, ax = plt.subplots(1, 1, figsize=(6, 4))
+
+a = np.linspace(-5, 5, 200)
+x = model.predict(a)
+
+ax.scatter(x_train, y_train, label='Train Data', c='red', s=0.2)
+ax.plot(a, x, label='Prediction')
+ax.plot(a, a**2, label='Ground Truth')
+
+
+ax.legend(loc='upper left')
+plt.show()
