f8f0b736e68741e64ca94dd229dfb2ef01ea88f1
Clemens <59970449+K9N6GM@users.noreply.github.com>
2022-01-07 02:13:23 +0100

    Update GGN_CG.py

--- GGN_CG.py
+++ GGN_CG.py
@@ -5,16 +5,14 @@ import tensorflow as tf
 import time
 
 import logging
-# random seed festlegen für das Training
-tf.random.set_seed(
-    1
-)
 tf.get_logger().setLevel(logging.ERROR)
+
+tf.random.set_seed(1)
 
 train_size = 1500
 test_size = 500
 batch_size = 100
-epochs = 200
+epochs = 20
 model_neurons = [1, 30, 30, 1]
 
 
@@ -47,17 +45,22 @@ bias_shape = [(model_neurons[i+1]) for i in range(np.shape(model_neurons)[0]-1)]
 param_shape = [x for y in zip(layer_shape, bias_shape) for x in y]
 n_params = [np.prod(s) for s in param_shape]
 ind = np.insert(np.cumsum(n_params), 0, 0)
+update_old = tf.zeros(ind[-1])
 lam = 1
+
 
 def fastmatvec(v, jac, lam):
     return tf.reduce_mean(tf.linalg.matvec(jac, tf.linalg.matvec(jac, v), transpose_a=True), axis=0) + lam * v
 
 
-def cg_method_new(jac, x, b, kmax, eps):
-    r = b #- fastmatvec(x, jac, lam)
+def cg_method(jac, x, b, min_steps, precision):  # Martens Werte: min_steps = 10, precision = 0.0005
+    r = b - fastmatvec(x, jac, lam)
     d = r
-    k = 0
-    while tf.math.reduce_euclidean_norm(r) > eps and k < kmax:
+    i, k = 0, min_steps
+    # Wie geht das schneller????
+    phi_history = np.array(- 0.5 * (tf.tensordot(x, b, 1) + tf.tensordot(x, r, 1)))
+    while (i > k and phi_history[-1] < 0 and s < precision*k) == False:
+        k = np.maximum(min_steps, int(i/min_steps))
         z = fastmatvec(d, jac, lam)
         alpha = tf.tensordot(r, r, 1) / tf.tensordot(d, z, 1)
         x = x + alpha * d
@@ -65,26 +68,63 @@ def cg_method_new(jac, x, b, kmax, eps):
         beta = tf.tensordot(r_new, r_new, 1) / tf.tensordot(r, r, 1)
         d = r_new + beta * d
         r = r_new
-        k += 1
-    return tf.reshape(x, [x.shape[0], 1])
+        phi_history = np.append(phi_history, np.array(
+            - 0.5 * (tf.tensordot(x, b, 1) + tf.tensordot(x, r, 1))))
+        if i >= k:
+            s = (phi_history[-1] - phi_history[-k]) / phi_history[-1]
+        else:
+            s = k
+        i += 1
+    return x
 
 
-def train_step_generalized_gauss_newton_new(x, y, lam):
+# Martens Werte: min_steps = 10, precision = 0.0005
+def preconditioned_cg_method(A, x, b, min_steps, precision):
+    r = b - fastmatvec(x, A, lam)
+    y = r / (b ** 2 + lam)
+    d = y
+    i, k = 0, min_steps
+    # Wie geht das schneller????
+    phi_history = np.array(- 0.5 * (tf.tensordot(x, b, 1) + tf.tensordot(x, r, 1)))
+    while (i > k and phi_history[-1] < 0 and s < precision*k) == False:
+        k = np.maximum(min_steps, int(i/min_steps))
+        z = fastmatvec(d, A, lam)
+        alpha = tf.tensordot(r, y, 1) / tf.tensordot(d, z, 1)
+        x = x + alpha * d
+        r_new = r - alpha * z
+        y_new = r_new / (b ** 2 + lam)
+        beta = tf.tensordot(r_new, y_new, 1) / tf.tensordot(r, y, 1)
+        d = y_new + beta * d
+        r = r_new
+        y = y_new
+        phi_history = np.append(phi_history, np.array(
+            - 0.5 * (tf.tensordot(x, b, 1) + tf.tensordot(x, r, 1))))
+        if i >= k:
+            s = (phi_history[-1] - phi_history[-k]) / phi_history[-1]
+        else:
+            s = k
+        i += 1
+
+    return x
+
+
+def train_step_generalized_gauss_newton(x, y, lam, update_old):
     with tf.GradientTape(persistent=True) as tape:
         y_pred = model(x)
         loss = model_loss(y, y_pred)
-        res = y_pred - y
-        if model_neurons[0] == 1:
-            res = tf.reshape(res, (batch_size, 1, 1))
+
+    res = y_pred - y
+    if model_neurons[0] == 1:
+        res = tf.reshape(res, (batch_size, 1, 1))
 
     theta = model.trainable_variables
     jac = tape.jacobian(y_pred, theta)
     jac = tf.concat([tf.reshape(h, [batch_size, model_neurons[-1], n_params[i]])
                      for i, h in enumerate(jac)], axis=2)
 
-    grad_obj = tf.reduce_mean(tf.matmul(jac, res, transpose_a=True), axis=0) #eventuell Listcompr.
+    grad_obj = tf.squeeze(tf.reduce_mean(tf.matmul(jac, res, transpose_a=True), axis=0))
 
-    update = cg_method_new(jac, tf.zeros(jac.shape[2]), tf.squeeze(grad_obj), 100, 1e-10)
+    update = preconditioned_cg_method(jac, tf.zeros(jac.shape[2]), grad_obj, 5, 0.0005)
 
     theta_new = [update[i:j] for (i, j) in zip(ind[:-1], ind[1:])]
 
@@ -94,14 +134,15 @@ def train_step_generalized_gauss_newton_new(x, y, lam):
 
     impr = loss - model_loss(y,  model(x))
 
-    rho = impr / (tf.reduce_sum(grad_obj * update) + tf.reduce_sum(tf.squeeze(update) * fastmatvec(tf.squeeze(update), jac, 0)))
+    rho = impr / (tf.tensordot(grad_obj, update, 1) +
+                  tf.tensordot(update, fastmatvec(update, jac, 0), 1))
 
     if rho > 0.75:
         lam /= 1.5
     elif rho < 0.25:
         lam *= 1.5
 
-    return lam
+    return lam, update
 
 
 def train_step_gradient_descent(x, y, eta):
@@ -130,15 +171,12 @@ for epoch in range(epochs):
         start = i * batch_size
         end = start + batch_size
 
-        lam = train_step_generalized_gauss_newton_new(x_train[start: end], y_train[start: end], lam)
+        lam, update_old = train_step_generalized_gauss_newton(
+            x_train[start: end], y_train[start: end], lam, update_old)
     #    train_step_gradient_descent(x_train[start: end], y_train[start: end], 0.3)
-print('finished training!')
+
 elapsed = time.time() - t
 
-
-#######
-#plots:
-#######
 
 #f, ax = plt.subplots(1, 1, figsize=(6, 4))
 
@@ -154,6 +192,6 @@ elapsed = time.time() - t
 #ax.set_xlim(-np.sqrt(10), np.sqrt(10))
 
 #ax.legend(loc='upper left')
-#plt.show()
+# plt.show()
 
 # https://sudonull.com/post/61595-Hessian-Free-optimization-with-TensorFlow
