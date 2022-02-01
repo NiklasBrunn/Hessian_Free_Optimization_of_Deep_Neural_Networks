cb2932e1a6f29d82e753ecef51b95c396626c5a6
NiklasBrunn <niklasbrunn@web.de>
2022-02-01 09:34:55 +0100

    R_Ops for MNIST-File (3 Methodes)

--- GGN_CG_MNIST.py
+++ GGN_CG_MNIST.py
@@ -8,10 +8,10 @@ from keras.datasets import mnist
 import logging
 tf.get_logger().setLevel(logging.ERROR)
 
-tf.random.set_seed(11)
+tf.random.set_seed(1)
 
 data_size = 60000
-batch_size = 150
+batch_size = 200
 epochs = 10
 model_neurons_mnist = [784, 800, 10]
 
@@ -31,39 +31,64 @@ def model_loss_mnist(y_true, y_pred):
     #return tf.reduce_mean(-tf.math.reduce_sum(y_true * tf.math.log(tf.nn.softmax(y_pred)), axis=1))
     return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_true, y_pred))
 
-def fastmatvec_new(x_batch, y_batch, v, lam):
+
+def fastmatvec_naiv(v, jac_net, jac_softmax, lam):
+    prod1 = tf.linalg.matvec(jac_net, v)
+    prod2 = tf.linalg.matvec(jac_softmax, prod1)
+    prod3 = tf.linalg.matvec(jac_net, prod2, transpose_a=True)
+    return tf.reduce_mean(prod3, axis=0) + lam * v
+
+
+def fastmatvec_V1(x_batch, y_batch, v, lam):
     v_new = [v[i:j] for (i, j) in zip(ind[:-1], ind[1:])]
     v_new = [tf.Variable(tf.reshape(u, s)) for (u, s) in zip(v_new, param_shape)]
-    with tf.autodiff.ForwardAccumulator(model.trainable_variables, v_new) as acc:
-        y_pred = model(x_batch)
+    with tf.autodiff.ForwardAccumulator(model_mnist.trainable_variables, v_new) as acc:
+        y_pred = model_mnist(x_batch)
     Jacnet_times_vec = acc.jvp(y_pred)
     with tf.GradientTape() as tape:
-        y_pred = model(x_batch)
+        y_pred = model_mnist(x_batch)
         with tf.autodiff.ForwardAccumulator(y_pred, Jacnet_times_vec) as acc1:
             akt_out = tf.nn.softmax(y_pred)
         Jac_softmax_times_vec = acc1.jvp(akt_out)
-    GGN_times_v = tape.gradient(y_pred, model.trainable_variables,
+    GGN_times_v = tape.gradient(y_pred, model_mnist.trainable_variables,
                              output_gradients=tf.stop_gradient(Jac_softmax_times_vec))
 
     v_new = tf.squeeze(tf.concat([tf.reshape(v, [n_params[i], 1])
                                   for i, v in enumerate(GGN_times_v)], axis=0))
-#    print(v_new/batch_size + lam * v)
+
     return v_new / batch_size + lam * v
 
 
-
-def fastmatvec_new_faster(x_batch, y_batch, v, lam):
+def fastmatvec_V2(x_batch, y_batch, v, lam):
     v_new = [v[i:j] for (i, j) in zip(ind[:-1], ind[1:])]
     v_new = [tf.Variable(tf.reshape(u, s)) for (u, s) in zip(v_new, param_shape)]
 
     with tf.GradientTape() as tape:
-        with tf.autodiff.ForwardAccumulator(model.trainable_variables, v_new) as acc:
-            y_pred = model(x_batch)
+        with tf.autodiff.ForwardAccumulator(model_mnist.trainable_variables, v_new) as acc:
+            y_pred = model_mnist(x_batch)
             akt_out = tf.nn.softmax(y_pred)
         Jsoft_v = acc.jvp(akt_out) # Jsoft_v = Jacobi_softmax(bzgl. Netzwerkparams) * v = ...
-        # ... = Jacobi_softmax(bzgl. Netzwerkoutp.) * Jacobi_netz(y_pred = model(x_batch)) * v
-    GGN_times_v = tape.gradient(y_pred, model.trainable_variables,
+        # ... = Jacobi_softmax(bzgl. Netzwerkoutp.) * Jacobi_netz(y_pred = model_mnist(x_batch)) * v
+    GGN_times_v = tape.gradient(y_pred, model_mnist.trainable_variables,
                              output_gradients=tf.stop_gradient(Jsoft_v))
+
+    v_new = tf.squeeze(tf.concat([tf.reshape(v, [n_params[i], 1])
+                                  for i, v in enumerate(GGN_times_v)], axis=0))
+
+    return v_new / batch_size + lam * v
+
+
+def fastmatvec_V3(x_batch, y_batch, v, lam):
+    v_new = [v[i:j] for (i, j) in zip(ind[:-1], ind[1:])]
+    v_new = [tf.Variable(tf.reshape(u, s)) for (u, s) in zip(v_new, param_shape)]
+
+    with tf.GradientTape() as tape:
+        with tf.autodiff.ForwardAccumulator(model_mnist.trainable_variables, v_new) as acc:
+            y_pred = model_mnist(x_batch)
+        Jnet_v = acc.jvp(y_pred)
+        akt_out = tf.nn.softmax(y_pred)
+    GGN_times_v = tape.gradient(akt_out, model_mnist.trainable_variables,
+                             output_gradients=tf.stop_gradient(Jnet_v))
 
     v_new = tf.squeeze(tf.concat([tf.reshape(v, [n_params[i], 1])
                                   for i, v in enumerate(GGN_times_v)], axis=0))
@@ -92,21 +117,15 @@ update_old = tf.zeros(ind[-1])
 lam = 1
 
 
-def fastmatvec(v, jac_net, jac_softmax, lam):
-    prod1 = tf.linalg.matvec(jac_net, v)
-    prod2 = tf.linalg.matvec(jac_softmax, prod1)
-    prod3 = tf.linalg.matvec(jac_net, prod2, transpose_a=True)
-    return tf.reduce_mean(prod3, axis=0) + lam * v
-
 # Martens Werte: min_steps = 10, precision = 0.0005
 def cg_method(jac, jac_softmax, x, b, min_steps, precision):
-    r = b - fastmatvec(x, jac, jac_softmax, lam)
+    r = b - fastmatvec_naiv(x, jac, jac_softmax, lam)
     d = r
     i, k = 0, min_steps
     phi_history = np.array(- 0.5 * (tf.tensordot(x, b, 1) + tf.tensordot(x, r, 1)))
     while (i > k and phi_history[-1] < 0 and s < precision*k) == False:
         k = np.maximum(min_steps, int(i/min_steps))
-        z = fastmatvec(d, jac, jac_softmax, lam)
+        z = fastmatvec_naiv(d, jac, jac_softmax, lam)
         alpha = tf.tensordot(r, r, 1) / tf.tensordot(d, z, 1)
         x = x + alpha * d
         r_new = r - alpha * z
@@ -125,14 +144,14 @@ def cg_method(jac, jac_softmax, x, b, min_steps, precision):
 
 # Martens Werte: min_steps = 10, precision = 0.0005
 def preconditioned_cg_method(A, B, x, b, min_steps, precision):
-    r = b - fastmatvec(x, A, B, lam)
+    r = b - fastmatvec_naiv(x, A, B, lam)
     y = r / (b ** 2 + lam)
     d = y
     i, k = 0, min_steps
     phi_history = np.array(- 0.5 * (tf.tensordot(x, b, 1) + tf.tensordot(x, r, 1)))
     while (i > k and phi_history[-1] < 0 and s < precision*k) == False:
         k = np.maximum(min_steps, int(i/min_steps))
-        z = fastmatvec(d, A, B, lam)
+        z = fastmatvec_naiv(d, A, B, lam)
         alpha = tf.tensordot(r, y, 1) / tf.tensordot(d, z, 1)
         x = x + alpha * d
         r_new = r - alpha * z
@@ -149,6 +168,71 @@ def preconditioned_cg_method(A, B, x, b, min_steps, precision):
             s = k
         i += 1
     return x
+
+
+def preconditioned_cg_method_R_Op(v, x_batch, y_batch, b, min_steps, precision):
+    #r = b - fastmatvec_V1(x_batch, y_batch, v, lam)
+    #r = b - fastmatvec_V2(x_batch, y_batch, v, lam)
+    r = b - fastmatvec_V3(x_batch, y_batch, v, lam)
+    y = r / (b ** 2 + lam)
+    d = y
+    i, k = 0, min_steps
+    phi_history = np.array(- 0.5 * (tf.tensordot(v, b, 1) + tf.tensordot(v, r, 1)))
+    while (i > k and phi_history[-1] < 0 and s < precision*k) == False:
+        k = np.maximum(min_steps, int(i/min_steps))
+        #z = fastmatvec_V1(x_batch, y_batch, d, lam)
+        #z = fastmatvec_V2(x_batch, y_batch, d, lam)
+        z = fastmatvec_V3(x_batch, y_batch, d, lam)
+        alpha = tf.tensordot(r, y, 1) / tf.tensordot(d, z, 1)
+        v = v + alpha * d
+        r_new = r - alpha * z
+        y_new = r_new / (b ** 2 + lam)
+        beta = tf.tensordot(r_new, y_new, 1) / tf.tensordot(r, y, 1)
+        d = y_new + beta * d
+        r = r_new
+        y = y_new
+        phi_history = np.append(phi_history, np.array(
+            - 0.5 * (tf.tensordot(v, b, 1) + tf.tensordot(v, r, 1))))
+        if i >= k:
+            s = (phi_history[-1] - phi_history[-k]) / phi_history[-1]
+        else:
+            s = k
+        i += 1
+    return v
+
+
+def train_step_generalized_gauss_newton_R_Op(x, y, lam, update_old):
+    theta = model_mnist.trainable_variables
+    with tf.GradientTape() as tape:
+        loss = model_loss_mnist(y, model_mnist(x))
+    grad_obj = tape.gradient(loss, theta)
+    grad_obj = tf.squeeze(tf.concat([tf.reshape(g, [n_params[i], 1]) for i, g in enumerate(grad_obj)], axis=0))
+
+    update = preconditioned_cg_method_R_Op(update_old, x, y, grad_obj, 10, 0.0005)
+
+    theta_new = [update[i:j] for (i, j) in zip(ind[:-1], ind[1:])]
+
+    theta_new = [p - tf.reshape(u, s) for (p, u, s) in zip(theta, theta_new, param_shape)]
+
+    model_mnist.set_weights(theta_new)
+
+    impr = loss - model_loss_mnist(y,  model_mnist(x))
+
+#    rho = impr / (tf.tensordot(grad_obj, update, 1) +
+#                  tf.tensordot(update, fastmatvec_V1(x, y, update, 0), 1))
+
+#    rho = impr / (tf.tensordot(grad_obj, update, 1) +
+#                  tf.tensordot(update, fastmatvec_V2(x, y, update, 0), 1))
+
+    rho = impr / (tf.tensordot(grad_obj, update, 1) +
+                  tf.tensordot(update, fastmatvec_V3(x, y, update, 0), 1))
+
+    if rho > 0.75:
+        lam /= 1.5
+    elif rho < 0.25:
+        lam *= 1.5
+
+    return lam, update
 
 
 
@@ -191,7 +275,7 @@ def train_step_generalized_gauss_newton(x, y, lam, update_old):
     impr = loss - model_loss_mnist(y,  model_mnist(x))
 
     rho = impr / (tf.tensordot(grad_obj, update, 1) +
-                  tf.tensordot(update, fastmatvec(update, jac, jac_softmax, 0), 1))
+                  tf.tensordot(update, fastmatvec_naiv(update, jac, jac_softmax, 0), 1))
 
     if rho > 0.75:
         lam /= 1.5
@@ -229,9 +313,27 @@ for epoch in range(epochs):
         start = i * batch_size
         end = start + batch_size
 
+#        t = time.time()
 #        lam, update_old = train_step_generalized_gauss_newton(
 #            train_x[start: end], train_y[start: end], lam, update_old)
-        train_step_gradient_descent(train_x[start: end], train_y[start: end], 0.01)
+#        elapsed = time.time() - t
+#        print('estimated time for one batch update in epoch {}/{}: {:.4f}.'.format(str(epoch +
+#                                                           1).zfill(len(str(epochs))), epochs, elapsed))
+
+        t = time.time()
+        lam, update_old = train_step_generalized_gauss_newton_R_Op(
+            train_x[start: end], train_y[start: end], lam, update_old)
+        elapsed = time.time() - t
+        print('estimated time for one batch update in epoch {}/{}: {:.4f}.'.format(str(epoch +
+                                                           1).zfill(len(str(epochs))), epochs, elapsed))
+
+
+#        t = time.time()
+#        train_step_gradient_descent(train_x[start: end], train_y[start: end], 0.01)
+#        elapsed = time.time() - t
+#        print('estimated time for one batch update in epoch {}/{}: {:.4f}.'.format(str(epoch +
+#                                                           1).zfill(len(str(epochs))), epochs, elapsed))
+
 
 #elapsed = time.time() - t
 
