433f7c04eac525cfb272bb5bc0a02bbafcd930a7
Clemens <59970449+K9N6GM@users.noreply.github.com>
2022-02-24 20:34:13 +0100

    Update train_steps.py

--- train_steps.py
+++ train_steps.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 
 # Model Loss functions
-def model_loss(data, y_true, y_pred):
+def model_loss(y_true, y_pred):
     if tf.shape(y_true)[-1] == 10:
         return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_true, y_pred))
     else:
